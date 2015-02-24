e0263e600cd6522fff963a791d99e84a9c3b940e
Sam Willcocks <sam@wlcx.cc>
2015-02-24 16:12:53 +0000

    Rewrite screen system
    Screens are now all their own structs which implement the SpotScreen interface
    Screens are initialised and placed in the screen slice in the spot struct
    Add SpotInit func which initialises screens and returns a spot instance ready to run
    Scrolllists now have a focus arg which changes colors
    Spotscreens are now responsible for handling some keypresses with the HandleTBEvent func

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ type ScrollList struct {
 	selected int
 }
 
-func (l *ScrollList) Draw(x, y, w, h int) {
+func (l *ScrollList) Draw(x, y, w, h int, focussed bool) {
 	// TODO: actually implement scrolling
 	for i := 0; i < h; i++ {
 		if i == len(l.items) {
@@ -73,7 +73,12 @@ func (l *ScrollList) Draw(x, y, w, h int) {
 		}
 		fgcolor, bgcolor := tb.ColorWhite, tb.ColorDefault
 		if i == l.selected { // Use selected colours
-			fgcolor, bgcolor = tb.ColorRed, tb.ColorBlack
+			bgcolor = tb.ColorBlack
+			if focussed {
+				fgcolor = tb.ColorYellow
+			} else {
+				fgcolor = tb.ColorRed
+			}
 		}
 		for ix := x; ix < x+w; ix++ {
 			tb.SetCell(ix, y+i, ' ', fgcolor, bgcolor)
@@ -117,22 +122,122 @@ const (
 	Search
 )
 
-type SpotScreen int
+type SpotScreen interface {
+	Draw(g *spot, x, y int)
+	HandleTBEvent(ev tb.Event)
+}
 
-const (
-	SpotScreenAbout SpotScreen = iota
-	SpotScreenPlaylists
-)
+type SpotScreenAbout struct{}
+
+func (SpotScreenAbout) Draw(_ *spot, _, _ int) {
+	printtb(8, 5, tb.ColorGreen, tb.ColorDefault, `                     __ `)
+	printtb(8, 6, tb.ColorGreen, tb.ColorDefault, `   _________  ____  / /_`)
+	printtb(8, 7, tb.ColorGreen, tb.ColorDefault, `  / ___/ __ \/ __ \/ __/`)
+	printtb(8, 8, tb.ColorGreen, tb.ColorDefault, ` (__  ) /_/ / /_/ / /_  `)
+	printtb(8, 9, tb.ColorGreen, tb.ColorDefault, `/____/ .___/\____/\__/  `)
+	printtb(8, 10, tb.ColorGreen, tb.ColorDefault, `    /_/                 `)
+	printtb(8, 12, tb.ColorWhite, tb.ColorDefault, "Version "+version)
+	printtb(40, 6, tb.ColorWhite, tb.ColorDefault, "Welcome to Spot!")
+	printtb(40, 7, tb.ColorWhite, tb.ColorDefault, "A simple, fast command line Spotify Client")
+	printtb(40, 9, tb.ColorWhite, tb.ColorDefault, "Login by typing")
+	printtb(40, 10, tb.ColorWhite, tb.ColorDefault, ":login <username> <password>")
+}
+
+func (SpotScreenAbout) HandleTBEvent(tb.Event) {
+}
+
+type SpotScreenPlaylists struct {
+	playlists      ScrollList
+	tracks         ScrollList
+	tracksfocussed bool // if false, playlist list is focussed
+}
+
+func (s *SpotScreenPlaylists) Draw(g *spot, x, y int) {
+	var playlistlist, tracklist []ListItem
+	if g.session.ConnectionState() == sp.ConnectionStateLoggedIn {
+		playlistcont, err := g.session.Playlists()
+		if err != nil {
+			g.cmdline.status = err.Error()
+		} else {
+			playlistcont.Wait()
+			indent := 0
+			for i := 0; i < playlistcont.Playlists(); i++ {
+				// This is a little fiddly, we have to deal with playlist
+				// folders as well as regular playlists
+				switch playlistcont.PlaylistType(i) {
+				case sp.PlaylistTypePlaylist:
+					playlistlist = append(playlistlist, ListItem{strings.Repeat(" ", indent) + playlistcont.Playlist(i).Name(), i})
+				case sp.PlaylistTypeStartFolder:
+					folder, _ := playlistcont.Folder(i)
+					playlistlist = append(playlistlist, ListItem{strings.Repeat(" ", indent) + folder.Name(), i})
+					indent++
+				case sp.PlaylistTypeEndFolder:
+					indent--
+				}
+			}
+			s.playlists.items = playlistlist
+			switch playlistcont.PlaylistType(s.playlists.items[s.playlists.selected].data) {
+			case sp.PlaylistTypePlaylist:
+				playlist := playlistcont.Playlist(s.playlists.items[s.playlists.selected].data)
+				playlist.Wait()
+				for i := 0; i < playlist.Tracks(); i++ {
+					tracklist = append(tracklist, ListItem{playlist.Track(i).Track().Name(), i})
+				}
+				s.tracks.items = tracklist
+			default:
+				s.tracks.items = nil
+			}
+		}
+	}
+	s.playlists.Draw(0, 1, 30, y-3, !s.tracksfocussed)
+	drawbox(30, 1, 1, y-3, "") // Dividing line
+	s.tracks.Draw(31, 1, x-31, y-3, s.tracksfocussed)
+}
+
+func (s *SpotScreenPlaylists) HandleTBEvent(ev tb.Event) {
+	switch ev.Key {
+	case tb.KeyTab:
+		// Swap focus between playlist and track scrolllists
+		s.tracksfocussed = !s.tracksfocussed
+	case tb.KeyArrowUp:
+		if s.tracksfocussed {
+			s.tracks.SelectUp()
+		} else {
+			s.playlists.SelectUp()
+		}
+	case tb.KeyArrowDown:
+		if s.tracksfocussed {
+			s.tracks.SelectDown()
+		} else {
+			s.playlists.SelectDown()
+		}
+	}
+
+}
 
 type spot struct {
 	session       *sp.Session
+	logger        *log.Logger
 	cmdline       CmdLine
 	quit          bool
-	logger        *log.Logger
 	mode          Mode
-	currentscreen SpotScreen
-	playlists     ScrollList
-	tracks        ScrollList
+	currentscreen int
+	screens       []SpotScreen
+}
+
+func SpotInit(logger *log.Logger, session *sp.Session) spot {
+	a := SpotScreenAbout{}
+	p := SpotScreenPlaylists{}
+	return spot{
+		session:       session,
+		logger:        logger,
+		cmdline:       CmdLine{},
+		quit:          false,
+		mode:          Normal,
+		currentscreen: 0,
+		screens:       []SpotScreen{&a, &p},
+	}
+
 }
 
 // (re)Draws the spot UI
@@ -148,61 +253,8 @@ func (g *spot) redraw() {
 	statusmsg := ConnstateMsg[g.session.ConnectionState()]
 	printtbrev(x, 0, statusmsg.Colour, tb.ColorBlack, statusmsg.Msg)
 
-	// Draw screens
-	switch g.currentscreen {
-	case SpotScreenAbout:
-		printtb(8, 5, tb.ColorGreen, tb.ColorDefault, `                     __ `)
-		printtb(8, 6, tb.ColorGreen, tb.ColorDefault, `   _________  ____  / /_`)
-		printtb(8, 7, tb.ColorGreen, tb.ColorDefault, `  / ___/ __ \/ __ \/ __/`)
-		printtb(8, 8, tb.ColorGreen, tb.ColorDefault, ` (__  ) /_/ / /_/ / /_  `)
-		printtb(8, 9, tb.ColorGreen, tb.ColorDefault, `/____/ .___/\____/\__/  `)
-		printtb(8, 10, tb.ColorGreen, tb.ColorDefault, `    /_/                 `)
-		printtb(8, 12, tb.ColorWhite, tb.ColorDefault, "Version "+version)
-		printtb(40, 6, tb.ColorWhite, tb.ColorDefault, "Welcome to Spot!")
-		printtb(40, 7, tb.ColorWhite, tb.ColorDefault, "A simple, fast command line Spotify Client")
-		printtb(40, 9, tb.ColorWhite, tb.ColorDefault, "Login by typing")
-		printtb(40, 10, tb.ColorWhite, tb.ColorDefault, ":login <username> <password>")
-	case SpotScreenPlaylists:
-		var playlistlist, tracklist []ListItem
-		if g.session.ConnectionState() == sp.ConnectionStateLoggedIn {
-			playlistcont, err := g.session.Playlists()
-			if err != nil {
-				g.cmdline.status = err.Error()
-			} else {
-				playlistcont.Wait()
-				indent := 0
-				for i := 0; i < playlistcont.Playlists(); i++ {
-					// This is a little fiddly, we have to deal with playlist
-					// folders as well as regular playlists
-					switch playlistcont.PlaylistType(i) {
-					case sp.PlaylistTypePlaylist:
-						playlistlist = append(playlistlist, ListItem{strings.Repeat(" ", indent) + playlistcont.Playlist(i).Name(), i})
-					case sp.PlaylistTypeStartFolder:
-						folder, _ := playlistcont.Folder(i)
-						playlistlist = append(playlistlist, ListItem{strings.Repeat(" ", indent) + folder.Name(), i})
-						indent++
-					case sp.PlaylistTypeEndFolder:
-						indent--
-					}
-				}
-				g.playlists.items = playlistlist
-				switch playlistcont.PlaylistType(g.playlists.items[g.playlists.selected].data) {
-				case sp.PlaylistTypePlaylist:
-					playlist := playlistcont.Playlist(g.playlists.items[g.playlists.selected].data)
-					playlist.Wait()
-					for i := 0; i < playlist.Tracks(); i++ {
-						tracklist = append(tracklist, ListItem{playlist.Track(i).Track().Name(), i})
-					}
-					g.tracks.items = tracklist
-				default:
-					g.tracks.items = nil
-				}
-			}
-		}
-		g.playlists.Draw(0, 1, 30, y-3)
-		drawbox(30, 1, 1, y-3, "") // Dividing line
-		g.tracks.Draw(31, 1, x-31, y-3)
-	}
+	// Draw active screen
+	g.screens[g.currentscreen].Draw(g, x, y)
 	// Draw nowplaying
 	drawbar(y-2, tb.ColorBlack)
 	printtb(0, y-2, tb.ColorBlue, tb.ColorBlack, "Artist - Album - Track")
@@ -300,17 +352,8 @@ func (g *spot) run() {
 						g.cmdline.Clear()
 					}
 					g.mode = Normal
-				case tb.KeyArrowUp:
-					switch g.currentscreen {
-					case SpotScreenPlaylists:
-						g.playlists.SelectUp()
-					}
-				case tb.KeyArrowDown:
-					switch g.currentscreen {
-					case SpotScreenPlaylists:
-						g.playlists.SelectDown()
-					}
-
+				case tb.KeyTab, tb.KeyArrowUp, tb.KeyArrowDown:
+					g.screens[g.currentscreen].HandleTBEvent(ev)
 				default:
 					if g.mode == Command && ev.Ch != 0 {
 						g.cmdline.AddChar(ev.Ch)
@@ -325,9 +368,9 @@ func (g *spot) run() {
 							//Quit
 							g.quit = true
 						case '0':
-							g.currentscreen = SpotScreenAbout
+							g.currentscreen = 0
 						case '1':
-							g.currentscreen = SpotScreenPlaylists
+							g.currentscreen = 1
 						}
 					}
 				}
@@ -373,13 +416,7 @@ func main() {
 	if err != nil {
 		logger.Fatal(err)
 	}
-	gs := spot{
-		logger:  logger,
-		quit:    false,
-		session: session,
-		cmdline: CmdLine{},
-		mode:    Normal,
-	}
-	gs.redraw()
-	gs.run()
+	s := SpotInit(logger, session)
+	s.redraw()
+	s.run()
 }
