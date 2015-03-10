16ec7278527bbaa45e72416dbcaafdb036037303
Sam Willcocks <sam@wlcx.cc>
2015-03-10 22:02:59 +0000

    Add bounds checks for seek

--- main.go
+++ main.go
@@ -175,6 +175,12 @@ func (p *SpotPlayer) AddElapsed(dur time.Duration) {
 }
 
 func (p *SpotPlayer) Seek(pos time.Duration) {
+	if pos < 0 {
+		pos = 0
+	}
+	if p.elapsed+pos > p.track.Duration() {
+		pos = p.track.Duration()
+	}
 	p.spplayer.Seek(pos)
 	p.aw.Flush()
 	p.elapsed = pos
